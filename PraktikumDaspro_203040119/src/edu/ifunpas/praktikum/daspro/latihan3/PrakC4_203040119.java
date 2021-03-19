ca5cecb948739373456aa0ba9563386d918be055
vo55my <hilmydhaifullah@gmail.com>
2021-03-19 19:12:19 +0700

    Update Codingan

--- PraktikumDaspro_203040119/src/edu/ifunpas/praktikum/daspro/latihan3/PrakC4_203040119.java
+++ PraktikumDaspro_203040119/src/edu/ifunpas/praktikum/daspro/latihan3/PrakC4_203040119.java
@@ -6,9 +6,10 @@ public class PrakC4_203040119 {
 
 	public static void main (String [] args) {
 		Scanner sc = new Scanner (System.in);
+		System.out.println("Masukkan Jenis Kelamin (L/P) : ");
 		String jKelamin = sc.next();
 		
-		if (jKelamin.equalsIgnoreCase("L") && jKelamin.equalsIgnoreCase("P")) {
+		if (jKelamin.equalsIgnoreCase("L") || jKelamin.equalsIgnoreCase("P")) {
 			System.out.println("Selamat Anda dalam keadaan baik");
 		} else {
 			System.out.println("Maaf, Jenis Kelamin anda apa??");
